8d21bf9e7cccd3de10ec7d4a4bb5f5d843587e4a
Léon-Pierre Dufour <leonpierre.dufour@gmail.com>
2024-09-29 08:24:36 -0400

    Update cmp.lua

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,73 +1,59 @@
 local M = {}
 
 local function init()
-	local cmp = require("cmp")
-	local luasnip = require("luasnip")
-	luasnip.config.setup({})
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-	cmp.setup({
-		view = {
-			entries = { name = "custom", selection_order = "near_cursor" },
-		},
-		snippet = {
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		},
-		window = {
-			completion = cmp.config.window.bordered(),
-		},
-		completion = { completeopt = "menu,menuone,noinsert" },
+    luasnip.config.setup({})
 
-		-- For an understanding of why these mappings were
-		-- chosen, you will need to read `:help ins-completion`
-		--
-		-- No, but seriously. Please read `:help ins-completion`, it is really good!
-		mapping = cmp.mapping.preset.insert({
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-y>"] = cmp.mapping.confirm({ select = true }),
-			["<C-Space>"] = cmp.mapping.complete({}),
-			["<C-l>"] = cmp.mapping(function()
-				if luasnip.expand_or_locally_jumpable() then
-					luasnip.expand_or_jump()
-				end
-			end, { "i", "s" }),
-			["<C-h>"] = cmp.mapping(function()
-				if luasnip.locally_jumpable(-1) then
-					luasnip.jump(-1)
-				end
-			end, { "i", "s" }),
-		}),
-		sources = {
-			{ name = "nvim_lsp" },
-			{ name = "buffer" },
-			{ name = "luasnip" },
-			{ name = "path" },
-		},
-	})
-	cmp.setup.cmdline("/", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = {
-			{ name = "buffer" },
-		},
-	})
-	-- `:` cmdline setup.
-	cmp.setup.cmdline(":", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "path" },
-		}, {
-			{
-				name = "cmdline",
-				option = {
-					ignore_cmds = { "Man", "!" },
-				},
-			},
-		}),
-	})
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-l>"] = cmp.mapping(function()
+                if luasnip.expand_or_locally_jumpable() then
+                    luasnip.expand_or_jump()
+                end
+            end, { "i", "s" }),
+            ["<C-h>"] = cmp.mapping(function()
+                if luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                end
+            end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "buffer" },
+            { name = "path" },
+        }),
+    })
+
+    -- Set up cmdline completions
+    cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {{ name = "buffer" }},
+    })
+
+    cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources(
+            {{ name = "path" }},
+            {{ name = "cmdline", option = { ignore_cmds = { "Man", "!" } } }}
+        ),
+    })
 end
 
 M.init = init
